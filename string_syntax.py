5472de9a72b12f556104903550341c509e95e16e
Cyle <cylemosley@gmail.com>
2024-01-02 18:48:54 -0500

    Revert "Add string_syntax.py"
    This reverts commit b52c1c31341f1c75d1a37c5e04c04101ed14d513.

--- string_syntax.py
+++
@@ -1,7 +0,0 @@
-phrase = "Hello world"  # Variables aren't declared in Python. It is a dynamically typed language.
-
-# note: '\' is an escape character which allows for typing characters that might break a code. ex: ( ) \ / " ' :
-
-print(phrase)  # Prints variable 'phrase'.
-
-print()
