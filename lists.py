3d3613c5426599e10dc4ac3588f17ace6289439d
Cyle <cylemosley@gmail.com>
2024-01-03 19:13:03 -0500

    Add final notes to lists.py for the day.

--- lists.py
+++ lists.py
@@ -26,3 +26,28 @@ print(people)
 
 people.append(42)  # Adds an index to the end of list 'people'. In this case it added the int '42'.
 print(people)
+
+people.insert(1, "Barbie")  # Inserts Barbie into index 2, and shifts all of index's up.
+print(people)
+
+people.remove("Aaron")  # Removes Aaron from the list.
+print(people)
+
+people.clear()  # Completely empties the list.
+print(people)
+
+numbers.pop()  # Removes the last index from the list.
+print(numbers)
+
+print(numbers.index(13))  # Prints the index where int 13 is located.
+
+numbers.append(13)
+
+print(numbers.count(13))  # Prints how many times '13' appears in the list 'numbers'.
+
+numbers.sort()  # Sorts the list in numerical/alphabetical order.
+
+numbers.reverse()  # Reverses a lists current order.
+
+numbers2 = numbers.copy()  # Copies the contents of numbers to numbers2.
+
