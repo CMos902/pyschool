817e5194b717ddc27d10325a0b2a9234bb92b58b
Cyle <cylemosley@gmail.com>
2024-01-04 10:30:52 -0500

    Delete *.py

--- getting_user_input.py
+++
@@ -1,3 +0,0 @@
-user_name = input("Enter your name: ")  # Requesting the user enter their name and assign it to variable 'user_name'.
-user_age = input("Enter your age: ")  # Requesting the user enter their age and assign it to variable 'user_age'.
-print("Hello, " + user_name + ". You are " + user_age + " years old.")
