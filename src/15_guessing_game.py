0d22b43efb2ee5605fbf2cc89e8504f8f61a8ecb
CMos902 <122231709+CMos902@users.noreply.github.com>
2024-01-06 00:16:07 -0500

    Add 15_guessing_game.py

--- src/15_guessing_game.py
+++ src/15_guessing_game.py
@@ -1,18 +1,24 @@
 """
-Game where a user guesses a predefined number until they guess correctly. But, they only have 3 attempts.
+Game where a user guesses a random number until they guess correctly. But, they only have 3 attempts.
 """
-num = "10"
+import random  # Import `random` module.
+
+secret_num = random.randrange(1, 10)  # Assign a random number 1-10 to `secret_number`
 guess = ""
 num_guess = 0
-guess_left = 2 - num_guess
 
-while guess != num:
+print("DEBUG: secret_num = " + str(secret_num))  # Just for debugging. Comment this line out to play for real.
+while guess != secret_num:  # Loop that runs until the `secret_number` is guessed.
     guess = input("Guess a number 1-10: ")
-    num_guess += 1
-    if num_guess == 3:
+    num_guess += 1  # Track the number of guesses made.
+    if guess == str(secret_num):  # Win condition.
+        print("Good job, you guessed correctly!")
+        break  # Breaks the loop.
+    elif num_guess == 3:  # Lose condition.
         print("You're out of guesses, better luck next time!")
         break
-    elif num_guess != 3 and guess == "10":
-        print("Good job, you win!")
     else:
-        print(str(guess_left) + " more guesses, try again!")
+        if num_guess < 2:  # Wrong guess, but a number of guesses still left.
+            print(str(3 - num_guess) + " more guesses. Try again!")
+        elif num_guess == 2:  # Last guess.
+            print("Last chance! Make it count.")
